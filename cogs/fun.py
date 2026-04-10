f5f1c8e930966ecd3dc6959acda3bef0486118f7
youngcoder45 <youngcoder45@gmail.com>
2026-04-11 00:38:40 +0530

    fix merge conflict

--- cogs/fun.py
+++ cogs/fun.py
@@ -63,7 +63,6 @@ class Fun(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.question_index = 0
         self._absolute_template_cache_bytes: Optional[bytes] = None
         self._absolute_template_cache_expires_at = 0.0
         self._absolute_template_cache_lock = asyncio.Lock()
@@ -153,17 +152,6 @@ class Fun(commands.Cog):
         )
         result.seek(0)
         return result
-
-    def get_next_question(self) -> dict[str, str]:
-
-        if self.question_index >= len(TRIVIA_QUESTIONS):
-            random.shuffle(TRIVIA_QUESTIONS)
-            self.question_index = 0
-
-        question = TRIVIA_QUESTIONS[self.question_index]
-        self.question_index += 1
-
-        return question
 
     async def _get_absolute_template_bytes(self) -> bytes:
         now = time.monotonic()
